82b09f98e1fbaf7b340d227449f2e1c8bff65b0e
hanwenkai <han_wenm@163.com>
2017-05-22 22:10:03 +0800

    modified string.py

--- string.py
+++ string.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-s1=72
-s2=85
-r=s1/s2*100
-print('s1/s2=%d%%'%r)
+L=[
+['apple','google','microsoft'],
+['java','python','ruby','php'],
+['adam','bart','lisa']
+]
+# echo apple
+print('%s'% L[0][0])
+# echo python
+print('%s'% L[1][1])
+# echo lisa
+print('%s'% L[2][2])
