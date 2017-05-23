8b43f2758cec06cb4c77e57c1ddd14cb2e426e2f
hanwenkai <han_wenm@163.com>
2017-05-23 20:08:19 +0800

    commit a python ferqinashu is string.py

--- string.py
+++ string.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-L=[
-['apple','google','microsoft'],
-['java','python','ruby','php'],
-['adam','bart','lisa']
-]
-# echo apple
-print('%s'% L[0][0])
-# echo python
-print('%s'% L[1][1])
-# echo lisa
-print('%s'% L[2][2])
+def triangles():
+    a,b=[1],[1,1]
+    yield a
+    while True:
+        yield b
+        insertCount=len(b)-1
+        temp=[]
+        for x in range(insertCount):
+            temp.append(b[x]+b[x+1])
+        temp.insert(0,1)
+        temp.append(1)
+        b=temp[:]  
+n=0
+for t in triangles():
+    print(t)
+    n=n+1
+    if n>=10:
+        break
